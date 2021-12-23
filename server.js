5b15334154c443dcf9c08f98f6d011c731abe369
Thomas Mueller <thomasmueller87@t-online.de>
2021-12-23 14:50:11 +0100

    mongoose successfully implemented

--- server.js
+++ server.js
@@ -1,51 +1,45 @@
-import express from "express";
-import { ObjectId } from "mongodb";
-import databaseClient from "./lib/db.client.js";
+import express from 'express';
+import mongoose from 'mongoose';
+import Dino from './models/dino.model.js';
 
 const server = express();
 
+mongoose.connect('mongodb://localhost:27017/flaky-dinosaurs');
+
 server.use(express.json());
-
-server.get("/dinos/:dinoId", async (req, res) => {
+//Alle Dinos holen
+server.get('/dinos', async (req, res) => {
+  const dinos = await Dino.find();
+  res.json(dinos);
+});
+//Einzelne Dinos mit ID laden
+server.get('/dinos/:dinoId', async (req, res) => {
   const dinoId = req.params.dinoId;
-
-  await databaseClient.connect();
-  const db = databaseClient.db("flaky-dinosaurs");
-  const collection = db.collection("dinosaurs");
-
-  const dino = await collection.findOne({ _id: ObjectId(dinoId) });
+  const dino = await Dino.findById(dinoId);
   res.json(dino);
 });
 
-server.get("/dinos", async (req, res) => {
-  await databaseClient.connect();
-  const db = databaseClient.db("flaky-dinosaurs");
-  const collection = db.collection("dinosaurs");
-
-  const dinos = await collection.find().toArray();
-  res.json(dinos);
-});
-
-server.post("/dinos", async (req, res) => {
-  const dinosaur = {
+//Erstelle neuen Dino
+server.post('/dinos', async (req, res) => {
+  const dinosaur = new Dino({
     name: req.body.name,
     type: req.body.type,
     vegan: req.body.vegan,
-  };
-
-  await databaseClient.connect();
-  const db = databaseClient.db("flaky-dinosaurs");
-
-  //Sammlung aller Dokumente
-  const collection = db.collection("dinosaurs");
-  // Füge ein neues Dokument ein, warte auf as Ergebnis
-  const result = await collection.insertOne(dinosaur);
-  // Ergebnis auswerten und ID an den Nutzer senden.
-  res.json({
-    message: "Successfully inserted a new dino with ID: " + result.insertedId,
   });
+
+  try {
+    const result = await dinosaur.save();
+    // Ergebnis auswerten und ID an den Nutzer senden.
+    res.json({
+      message:
+        'Successfully inserted a new dino with ID: ' +
+        result._Id,
+    });
+  } catch (error) {
+    res.json(error);
+  }
 });
 
 server.listen(4000, () => {
-  console.log("Dino-Server is up and running");
+  console.log('Dino-Server is up and running');
 });
