b45c0c52a274925af9cd27b1b0e18a8e19f72cac
Thomas Mueller <thomasmueller87@t-online.de>
2022-01-03 15:23:38 +0100

    dotenv added

--- server.js
+++ server.js
@@ -1,17 +1,31 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import cors from 'cors';
+import dotenv from 'dotenv';
 
 import productsRoutes from './routes/products.routes.js';
 import catergoryRoutes from './routes/categories.routes.js';
 
+dotenv.config();
+
 const server = express();
 //Server Port
-server.listen(4000, () => {
-  console.log('Xmas-Server is up and running on Port 4000');
+const serverPort = process.env.SERVER_PORT || 4000;
+server.listen(serverPort, () => {
+  console.log(
+    `Xmas-Server is up and running on Port ${serverPort}`
+  );
 });
 
-const connectionString = 'mongodb://localhost:27017/xmas-app';
+const dbUser = process.env.DB_USER;
+const dbPassword = process.env.DB_PASSWORD;
+const dbHost = process.env.DB_HOST;
+const dbName = process.env.DB_NAME;
+
+const connectionString = `mongodb+srv://${dbUser}:${dbPassword}@${dbHost}/${dbName}?retryWrites=true&w=majority`;
+
+//For connect to localhost
+//const connectionString = 'mongodb://localhost:27017/xmas-app';
 
 mongoose.connect(connectionString);
 
